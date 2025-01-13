2ba8ef25ed94ac96ab1c77960dabaa508a3a2d13
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-01-13 20:34:18 +0100

    feat: Update for Forge 1.21.4

--- forge/src/main/java/net/blay09/mods/farmingforblockheads/ForgeFarmingForBlockheads.java
+++ forge/src/main/java/net/blay09/mods/farmingforblockheads/ForgeFarmingForBlockheads.java
@@ -1,17 +1,20 @@
 package net.blay09.mods.farmingforblockheads;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
+import net.blay09.mods.balm.forge.ForgeLoadContext;
 import net.blay09.mods.farmingforblockheads.client.FarmingForBlockheadsClient;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(FarmingForBlockheads.MOD_ID)
 public class ForgeFarmingForBlockheads {
-    public ForgeFarmingForBlockheads() {
-        Balm.initialize(FarmingForBlockheads.MOD_ID, EmptyLoadContext.INSTANCE, FarmingForBlockheads::initialize);
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(FarmingForBlockheads.MOD_ID, EmptyLoadContext.INSTANCE, FarmingForBlockheadsClient::initialize));
+    public ForgeFarmingForBlockheads(FMLJavaModLoadingContext context) {
+        final var loadContext = new ForgeLoadContext(context.getModEventBus());
+        Balm.initialize(FarmingForBlockheads.MOD_ID, loadContext, FarmingForBlockheads::initialize);
+        if (FMLEnvironment.dist.isClient()) {
+            BalmClient.initialize(FarmingForBlockheads.MOD_ID, loadContext, FarmingForBlockheadsClient::initialize);
+        }
     }
 }
